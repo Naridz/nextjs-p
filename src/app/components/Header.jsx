e85dc7117f7487c4ea62064b936dc3fb142c8b91
Naridz <naris2001@gmail.com>
2024-07-05 17:18:01 +0700

    555

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -21,8 +21,8 @@ function Header() {
     <header className='bg-gradient-to-r from-indigo-500 
     from-10% via-sky-500 via-30% to-emerald-500 to-90% h-[300px] flex justify-center items-center'>
         <div className='text-center'>
-            <h1 className='text-5xl'>NextJs Pokemon Collections</h1>
-            <p className='text-2xl'>Find your Pokemon !</p>
+            <h1 className='text-5xl'>Pokemon With NextJs</h1>
+            <p className='text-2xl'>Find Pokemon !</p>
             <form onSubmit={handleForm} className='flex mt-2'>
                 <input 
                  type='text'
@@ -30,7 +30,11 @@ function Header() {
                  placeholder='Search . . .'
                  onChange={handleSearch}
                 />
-                <button className='inline-flex items-center mx-2 px-4 py-2 bg-blue-800 text-white rounded-md shadow-md' type='submit'>Search</button>
+                {poName?(
+                  <button className='inline-flex items-center mx-2 px-4 py-2 bg-blue-800 text-white rounded-md shadow-md' type='submit'>Search</button>
+                ):(
+                  <button className='inline-flex items-center mx-2 px-4 py-2 bg-blue-700 text-white rounded-md shadow-md' disabled type='submit'>Search</button>
+                )}
             </form>
         </div>
     </header>
